d2c6dde9d993cb663cdc26e4cfec5aca803e0b45
Kitsaponjim <98834570+Kitsaponjim@users.noreply.github.com>
2023-01-20 11:33:58 +0700

    Add all file of Project
    Test

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('MyApp');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+
+    res.send("Hello Jim") 
+
+})
+
+app.listen(port, ()=>{
+    console.log("Listening on port : "+ chalk.green(port));
+})
